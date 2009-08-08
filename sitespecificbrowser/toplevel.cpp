0a7d5c51ceafafff3d19587fba59db9218cfce25
Richard Moore <rich@kde.org>
2009-08-08 17:24:41 +0100

    - Move settings into a settings object. - Add support for loading and saving the settings. - Add support for multiple valid bases per-application.

--- sitespecificbrowser/toplevel.cpp
+++ sitespecificbrowser/toplevel.cpp
@@ -3,20 +3,29 @@
 #include <QtGui/QDesktopServices>
 #include <qwebsettings.h>
 #include <qwebframe.h>
+#include <qsettings.h>
+#include <qdir.h>
 
 #include "toplevel.h"
 
-Page::Page()
-    : QWebPage(0) 
+Page::Page( TopLevel *toplevel )
+    : QWebPage( toplevel ) 
 {
+    this->toplevel = toplevel;
 }
 
 bool Page::acceptNavigationRequest( QWebFrame *frame, const QNetworkRequest &request, QWebPage::NavigationType type )
 {
     if ( type == QWebPage::NavigationTypeLinkClicked ) {
-	QUrl base("http://mail.google.com/");
-	if ( !base.isParentOf( request.url() ) ) {
-	    qDebug() << "New browser for navigation to " << request.url();
+	bool inScope = false;
+	QUrl base;
+	foreach( base, toplevel->options()->allowedBases ) {
+	    if ( base.isParentOf( request.url() ) ) {
+		inScope = true;
+	    }
+	}
+
+	if ( !inScope ) {
 	    QDesktopServices::openUrl( request.url() );
 	    return false;
 	}
@@ -25,32 +34,79 @@ bool Page::acceptNavigationRequest( QWebFrame *frame, const QNetworkRequest &req
     return QWebPage::acceptNavigationRequest( frame, request, type );
 }
 
-/** Stuff that should be per-webapp
-struct WebAppOptions
-{
-    QUrl startUrl;
-    QList<QUrl> allowedBases;
-    QIcon windowIcon;
-    QString windowTitle;
-    QMap<QString, QString> jsActions;
-};
-*/
 
 TopLevel::TopLevel()
     : QWebView(0)
 {
-    m_page = new Page();
+    m_options = new WebAppOptions;
+#if 0
+    load( QString("gmail.ini") );
+#else
+    m_options->startUrl = QUrl("http://mail.google.com/");
+    m_options->windowTitle = QString("GMail");
+    m_options->allowedBases.append( QUrl("http://mail.google.com/") );
+
+    save( QString("gmail.ini") ); // HACK
+#endif
+
+    m_page = new Page( this );
+    connect( m_page->mainFrame(), SIGNAL( iconChanged() ), SLOT( iconLoaded() ) );
     setPage( m_page );
 
     QWebSettings::globalSettings()->setAttribute( QWebSettings::PluginsEnabled, true );
-    QWebSettings::setIconDatabasePath( QString("./icons") );
-    QIcon icon = QWebSettings::iconForUrl( QUrl("http://mail.google.com/") );
-    qDebug() << icon;
+    QWebSettings::setIconDatabasePath( QDir::currentPath() );
 
-    setWindowTitle( QString("GMail") );
-    setUrl( QUrl("http://mail.google.com/") );
+    setWindowTitle( m_options->windowTitle );
+    setUrl( m_options->startUrl );
 
-    connect( m_page->mainFrame(), SIGNAL( iconChanged() ), SLOT( iconLoaded() ) );
+       QIcon icon = QWebSettings::iconForUrl( QUrl("http://mail.google.com/mail/") );
+    //QIcon icon = m_page->mainFrame()->icon();
+    qDebug() << "Is icon null: " << icon.isNull();
+    if ( !icon.isNull() )
+	setWindowIcon( icon );
+
+}
+
+WebAppOptions *TopLevel::options() const
+{
+    return m_options;
+}
+
+bool TopLevel::load( const QString &filename )
+{
+    // TODO
+    QSettings settings( filename, QSettings::IniFormat );
+
+    m_options->startUrl = QUrl( settings.value( QString("StartUrl") ).toString() );
+    m_options->windowTitle = settings.value( QString("WindowTitle") ).toString();
+
+    int size = settings.beginReadArray( QString("AllowedBases") );
+    for ( int i = 0; i < size; ++i ) {
+	settings.setArrayIndex( i );
+	m_options->allowedBases.append( QUrl( settings.value( QString("Url") ).toString() ) );
+    }
+    settings.endArray();
+
+    return true;
+}
+
+bool TopLevel::save( const QString &filename )
+{
+    QSettings settings( filename, QSettings::IniFormat );
+
+    settings.setValue( QString("StartUrl"), m_options->startUrl.toString() );
+    settings.setValue( QString("WindowTitle"), m_options->windowTitle );
+
+    settings.beginWriteArray( QString("AllowedBases") );
+    for ( int i = 0; i < m_options->allowedBases.size(); ++i ) {
+	settings.setArrayIndex( i );
+	settings.setValue( QString("Url"), m_options->allowedBases[i].toString() );
+    }
+    settings.endArray();
+
+    settings.sync();
+
+    return true;
 }
 
 void TopLevel::iconLoaded()
--- sitespecificbrowser/toplevel.h
+++ sitespecificbrowser/toplevel.h
@@ -3,16 +3,32 @@
 
 #include <qwebview.h>
 #include <qwebpage.h>
+#include <qmap.h>
+
+class TopLevel;
+
+/** Stuff that should be per-webapp */
+struct WebAppOptions
+{
+    QUrl startUrl;
+    QList<QUrl> allowedBases;
+    QIcon windowIcon;
+    QString windowTitle;
+    QMap<QString, QString> jsActions;
+};
 
 class Page : public QWebPage
 {
     Q_OBJECT
 
 public:
-    Page();
+    Page( TopLevel *toplevel );
 
 protected:
     bool acceptNavigationRequest( QWebFrame *frame, const QNetworkRequest &request, QWebPage::NavigationType type );
+
+private:
+    TopLevel *toplevel;
 };
 
 class TopLevel : public QWebView
@@ -22,10 +38,16 @@ class TopLevel : public QWebView
 public:
     TopLevel();
 
+    bool load( const QString &filename );
+    bool save( const QString &filename );
+
+    WebAppOptions *options() const;
+
 protected slots:
     void iconLoaded();
 
 private:
+    struct WebAppOptions *m_options;
     Page *m_page;
 };
 
