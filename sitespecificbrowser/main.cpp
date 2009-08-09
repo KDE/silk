44a7f20e62adf794aa313bf5dccb639537acd3e9
Sebastian Kuegler <sebas@kde.org>
2009-08-09 02:36:14 +0200

    Make it a KApplication

--- sitespecificbrowser/main.cpp
+++ sitespecificbrowser/main.cpp
@@ -1,14 +1,41 @@
-#include <qapplication.h>
+
+// Qt
 #include <qboxlayout.h>
 #include <qtoolbar.h>
 #include <qwidget.h>
 
+// KDE
+#include <kapplication.h>
+#include <kaboutdata.h>
+#include <kcmdlineargs.h>
+#include <KDE/KLocale>
+
+// Own
 #include "view.h"
 
-int main( int argc, char **argv )
-{
-    QApplication app( argc, argv );
+static const char description[] =
+I18N_NOOP("A KDE 4 Application");
 
+static const char version[] = "0.1";
+
+int main(int argc, char **argv)
+{
+    KAboutData about("sitespecificbrowser", 0, ki18n("sitespecificbrowser"), version, ki18n(description),
+                     KAboutData::License_GPL, ki18n("(C) 2007 Richard Moore"), KLocalizedString(), 0, "rich@kde.org");
+                     about.addAuthor( ki18n("Richard Moore"), KLocalizedString(), "rich@kde.org" );
+    KCmdLineArgs::init(argc, argv, &about);
+
+    KCmdLineOptions options;
+    options.add("+[URL]", ki18n( "Website to open" ));
+    KCmdLineArgs::addCmdLineOptions(options);
+    KApplication app;
+
+    KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+    if (args->count() == 0)
+    {
+        //sitespecificbrowser *widget = new sitespecificbrowser;
+        //widget->show();
+    }
     QWidget *w = new QWidget();
     QToolBar *bar = new QToolBar(w);
     bar->setToolButtonStyle( Qt::ToolButtonTextUnderIcon );
@@ -24,5 +51,8 @@ int main( int argc, char **argv )
     box->addWidget(&tl);
 
     w->show();
-    app.exec();
-};
+
+    args->clear();
+
+    return app.exec();
+}
