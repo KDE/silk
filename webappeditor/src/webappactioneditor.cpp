dc7c818fdacf6bc532114cefe926db35e9db6fc1
Sebastian Kuegler <sebas@kde.org>
2009-09-25 03:45:06 +0200

    Factor out the webappactioneditor

---
+++ webappeditor/src/webappactioneditor.cpp
@@ -0,0 +1,202 @@
+/*
+ * webappactioneditor.cpp
+ *
+ * Copyright (C) 2009 Sebastian Kuegler <sebas@kde.org>
+ */
+#include "webappactioneditor.h"
+#include "settings.h"
+
+#include <QtGui/QDropEvent>
+#include <QtGui/QBoxLayout>
+#include <QtGui/QLineEdit>
+#include <QtGui/QListWidget>
+#include <QtGui/QPainter>
+
+#include <kconfigdialog.h>
+#include <kstatusbar.h>
+#include <KDesktopFile>
+#include <KFileDialog>
+#include <KPageDialog>
+#include <KPushButton>
+
+#include <kaction.h>
+#include <kactioncollection.h>
+#include <kstandardaction.h>
+
+#include <KDE/KLocale>
+
+WebAppActionEditor::WebAppActionEditor(QWidget *parent)
+    : QWidget(parent)
+{
+    // accept dnd
+    setAcceptDrops(true);
+
+    setupMainWidget();
+}
+
+WebAppActionEditor::~WebAppActionEditor()
+{
+}
+
+void WebAppActionEditor::setupMainWidget()
+{
+    actionUi.setupUi(this);
+
+
+    connect(actionUi.showOnUrlLine, SIGNAL(returnPressed()), this, SLOT(addShowOnUrlLine()));
+    connect(actionUi.triggerOnUrlLine, SIGNAL(returnPressed()), this, SLOT(addTriggerOnUrlLine()));
+    connect(actionUi.showOnWildcardLine, SIGNAL(returnPressed()), this, SLOT(addShowOnWildcardLine()));
+    connect(actionUi.triggerOnWildcardLine, SIGNAL(returnPressed()), this, SLOT(addTriggerOnWildcardLine()));
+
+    connect(actionUi.showOnUrl, SIGNAL(itemDoubleClicked(QListWidgetItem*)), this, SLOT(removeShowOnUrlItem(QListWidgetItem*)));
+    connect(actionUi.triggerOnUrl, SIGNAL(itemDoubleClicked(QListWidgetItem*)), this, SLOT(removeItem(QListWidgetItem*)));
+    connect(actionUi.showOnWildcard, SIGNAL(itemDoubleClicked(QListWidgetItem*)), this, SLOT(removeItem(QListWidgetItem*)));
+    connect(actionUi.triggerOnWildcard, SIGNAL(itemDoubleClicked(QListWidgetItem*)), this, SLOT(removeItem(QListWidgetItem*)));
+
+    connect(actionUi.saveButton, SIGNAL(clicked()), this, SLOT(save()));
+
+    // FIXME: temporary ...
+    m_actionFile = "/home/sebas/kdesvn/src/project-silk/selkie/services/test/silk-webapp-test-editor.desktop";
+    showActionFile();
+    kDebug() << "actionFile" << m_actionFile;
+}
+
+void WebAppActionEditor::removeShowOnUrlItem(QListWidgetItem *item)
+{
+    kDebug() << "removing item" << item->text();
+    actionUi.showOnUrl->removeItemWidget(item);
+    actionUi.showOnUrl->removeItemWidget(actionUi.showOnUrl->currentItem());
+}
+
+void WebAppActionEditor::removeItem(QListWidgetItem *item)
+{
+    KListWidget *lw = qobject_cast<KListWidget*>(sender());
+    if (lw) {
+        actionUi.showOnUrl->removeItemWidget(item);
+    }
+}
+
+void WebAppActionEditor::addShowOnUrlLine()
+{
+    QString text = actionUi.showOnUrlLine->text();
+    QUrl url(text);
+    if (url.isValid() && !getItems(actionUi.showOnUrl).contains(text)) {
+        actionUi.showOnUrl->addItem(text);
+    } else {
+        kWarning() << "Not a valid URL, won't save it:" << text;
+    }
+}
+
+void WebAppActionEditor::addTriggerOnUrlLine()
+{
+    QString text = actionUi.triggerOnUrlLine->text();
+    QUrl url(text);
+    if (url.isValid() && !getItems(actionUi.triggerOnUrl).contains(text)) {
+        actionUi.triggerOnUrl->addItem(text);
+    } else {
+        kWarning() << "Not a valid URL, won't save it:" << text;
+    }
+}
+
+void WebAppActionEditor::addShowOnWildcardLine()
+{
+    QString text = actionUi.showOnWildcardLine->text();
+    if (!text.isEmpty() && !getItems(actionUi.showOnWildcard).contains(text)) {
+        actionUi.showOnWildcard->addItem(text);
+    }
+}
+
+void WebAppActionEditor::addTriggerOnWildcardLine()
+{
+    QString text = actionUi.triggerOnWildcardLine->text();
+    if (!text.isEmpty() && !getItems(actionUi.triggerOnWildcard).contains(text)) {
+        actionUi.triggerOnWildcard->addItem(text);
+    }
+}
+
+
+QStringList WebAppActionEditor::getItems(QListWidget *listWidget)
+{
+    QStringList list;
+    int rows = listWidget->model()->rowCount();
+    for (int i = 0; i < rows; i++) {
+        QString text = listWidget->item(i)->text();
+        kDebug() << "Row:" << i << text;
+        list << text;
+    }
+    return list;
+}
+
+void WebAppActionEditor::setItems(QListWidget *listWidget, const QStringList &list)
+{
+    foreach (const QString &u, list) {
+        listWidget->addItem(u);
+    }
+}
+
+/*
+void WebAppActionEditor::setupActions()
+{
+    KStandardAction::open(this, SLOT(openActionFile()), actionCollection());
+    KStandardAction::quit(qApp, SLOT(closeAllWindows()), actionCollection());
+}
+*/
+void WebAppActionEditor::openActionFile()
+{
+    m_actionFile = KFileDialog::getOpenFileName(KUrl("file:///home/sebas/kdesvn/src/project-silk/selkie/services/silk"), QString("*.desktop"));
+    showActionFile();
+}
+
+void WebAppActionEditor::showActionFile()
+{
+    actionUi.title->setText(i18nc("title widget", "WebApp Action Editor (%1)", KUrl(m_actionFile).fileName()));
+    kDebug() << m_actionFile;
+    m_desktopFile = new KDesktopFile(m_actionFile);
+    KConfigGroup group = m_desktopFile->group("Desktop Entry");
+
+    actionUi.label->setText(group.readEntry("Name", QString()));
+    actionUi.pluginName->setText(group.readEntry("X-KDE-PluginInfo-Name", QString()));
+    actionUi.icon->setIcon(group.readEntry("Icon", QString()));
+    actionUi.author->setText(group.readEntry("Author", QString()));
+    setItems(actionUi.showOnUrl, group.readEntry("X-Silk-ShowOnUrl", QStringList()));
+    setItems(actionUi.triggerOnUrl, group.readEntry("X-Silk-TriggerOnUrl", QStringList()));
+    setItems(actionUi.showOnWildcard, group.readEntry("X-Silk-ShowOnWildcard", QStringList()));
+    setItems(actionUi.triggerOnWildcard, group.readEntry("X-Silk-TriggerOnWildcard", QStringList()));
+    actionUi.title->setPixmap(KIcon(actionUi.icon->icon()));
+    actionUi.saveButton->setIcon(KIcon("document-save"));
+
+    dump();
+}
+
+void WebAppActionEditor::save()
+{
+    // ...
+    kDebug() << "---> Saving here ...";
+    KConfigGroup group = m_desktopFile->group("Desktop Entry");
+    group.writeEntry("Icon", actionUi.icon->icon());
+    group.writeEntry("Author", actionUi.author->text());
+    group.writeEntry("Name", actionUi.label->text());
+    group.writeEntry("X-KDE-PluginInfo-Name", actionUi.pluginName->text());
+    group.writeEntry("X-Silk-ShowOnUrl", getItems(actionUi.showOnUrl));
+    group.writeEntry("X-Silk-TriggerOnUrl", getItems(actionUi.triggerOnUrl));
+    group.writeEntry("X-Silk-ShowOnWildcard", getItems(actionUi.showOnWildcard));
+    group.writeEntry("X-Silk-TriggerOnWildcard", getItems(actionUi.triggerOnWildcard));
+    m_desktopFile->sync();
+    actionUi.title->setPixmap(KIcon(actionUi.icon->icon()));
+    dump();
+}
+
+void WebAppActionEditor::dump()
+{
+    kDebug() << "------------------------------";
+    kDebug() << "Name" << m_desktopFile->readName() << actionUi.label->text();
+    kDebug() << "Icon" << m_desktopFile->readIcon() << actionUi.icon->icon();
+    kDebug() << "Type" << m_desktopFile->readType();
+    kDebug() << "shows:" << getItems(actionUi.showOnUrl);
+    kDebug() << "triggers:" << getItems(actionUi.triggerOnUrl);
+    kDebug() << "shows (wildcards):" << getItems(actionUi.showOnWildcard);
+    kDebug() << "triggers (wildcards):" << getItems(actionUi.triggerOnWildcard);
+}
+
+
+#include "webappactioneditor.moc"
---
+++ webappeditor/src/webappactioneditor.h
@@ -0,0 +1,77 @@
+/*
+ * webappactioneditor.h
+ *
+ * Copyright (C) 2008 Sebastian Kuegler <sebas@kde.org>
+ */
+#ifndef WEBAPPACTIONEDITOR_H
+#define WEBAPPACTIONEDITOR_H
+
+
+#include <kxmlguiwindow.h>
+#include <KPageDialog>
+//#include "ui_prefs_base.h"
+#include "ui_webappaction.h"
+
+class QListWidget;
+class KDesktopFile;
+class KPageDialog;
+class KPushButton;
+class KToggleAction;
+class KUrl;
+
+/**
+ * This class serves as the main window for WebAppActionEditor.  It handles the
+ * menus, toolbars, and status bars.
+ *
+ * @short Main window class
+ * @author Sebastian K?gler <sebas@kde.org>
+ * @version 0.1
+ */
+class WebAppActionEditor : public QWidget
+{
+    Q_OBJECT
+public:
+    /**
+     * Default Constructor
+     */
+    WebAppActionEditor(QWidget *parent = 0);
+
+    /**
+     * Default Destructor
+     */
+    virtual ~WebAppActionEditor();
+
+private Q_SLOTS:
+    void openActionFile();
+    //void optionsPreferences();
+    void save();
+
+    void addShowOnUrlLine();
+    void addTriggerOnUrlLine();
+    void addShowOnWildcardLine();
+    void addTriggerOnWildcardLine();
+
+    void removeItem(QListWidgetItem *item);
+    void removeShowOnUrlItem(QListWidgetItem *item);
+
+private:
+    //void setupActions();
+
+private:
+    void setupMainWidget();
+    void showActionFile();
+    QStringList getItems(QListWidget *listWidget);
+    void setItems(QListWidget *listWidget, const QStringList &list);
+    void dump();
+    Ui::WebAppActionEditor actionUi;
+
+    QString m_actionFile;
+    QGridLayout *m_layout;
+    QWidget *m_widget;
+
+    KDesktopFile *m_desktopFile;
+    KToggleAction *m_toolbarAction;
+    KToggleAction *m_statusbarAction;
+};
+
+#endif // WEBAPPACTIONEDITOR_H_
