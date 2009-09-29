37ede681f11367cc4f0ca23c6e014b859fce4bac
Sebastian Kuegler <sebas@kde.org>
2009-09-29 23:09:39 +0200

    cooler name

--- webappeditor/src/main.cpp
+++ webappeditor/src/main.cpp
@@ -11,7 +11,7 @@ static const char version[] = "0.1";
 
 int main(int argc, char **argv)
 {
-    KAboutData about("webappeditor", 0, ki18n("webappeditor"), version, ki18n(description),
+    KAboutData about("webappeditor", 0, ki18n("Selkie Remixer"), version, ki18n(description),
                      KAboutData::License_GPL, ki18n("(C) 2009 Sebastian K?gler"), KLocalizedString(), 0, "sebas@kde.org");
     about.addAuthor( ki18n("Sebastian Kügler"), KLocalizedString(), "sebas@kde.org" );
     KCmdLineArgs::init(argc, argv, &about);
