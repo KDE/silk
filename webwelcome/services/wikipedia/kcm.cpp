bd64873808cdf864cf8f9b8b43bfc2194eac5a82
Sebastian Kuegler <sebas@luna.vizZzion.net>
2010-05-21 19:24:23 +0200

    Twitter KCM loading works as well
    Now we can start filling these KCMs up with sensible information. And if
    we go on in this pace, the whole webwelcome will be done by tomorrow.
    Something must be wrong ;-)

--- webwelcome/services/wikipedia/kcm.cpp
+++ webwelcome/services/wikipedia/kcm.cpp
@@ -1,3 +1,22 @@
+/*
+    Copyright 2010 by Sebastian Kügler <sebas@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
 
 #include <QLabel>
 
@@ -25,7 +44,7 @@ KcmWikipedia::KcmWikipedia(QWidget *parent, const QVariantList &)
   : KCModule(KcmWikipediaFactory::componentData(), parent)
 {
   setButtons(Help);
-  setAboutData(new KAboutData("kcmjoystick", 0, ki18n("Wikipedia Setup"), "0.1",
+  setAboutData(new KAboutData("kcmwikipedia", 0, ki18n("Wikipedia Setup"), "0.1",
                                ki18n("Configure your system for use with Wikipedia"),
                                KAboutData::License_GPL, ki18n("(c) 2010, Sebastian Kügler"),
                                KLocalizedString(), "sebas@kde.org"));
