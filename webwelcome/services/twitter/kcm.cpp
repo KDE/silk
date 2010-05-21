4e2dd79ee9289ca3ff2afe28a2cf48a7e2146876
Sebastian Kuegler <sebas@luna.vizZzion.net>
2010-05-21 21:34:27 +0200

    those KCMs are needed as well (even doesn't work for twitter yet)

---
+++ webwelcome/services/twitter/kcm.cpp
@@ -0,0 +1,82 @@
+/*
+    Copyright 2010 by Sebastian Kügler <sebas@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+
+#include <QLabel>
+
+#include "kcm.h"
+
+#include <kaboutdata.h>
+#include <kglobal.h>
+#include <klocale.h>
+#include <kdialog.h>
+
+#include <stdio.h>
+#include <KPluginFactory>
+#include <KPluginLoader>
+
+#include <QVBoxLayout>
+
+K_PLUGIN_FACTORY(KcmTwitterFactory,
+        registerPlugin<KcmTwitter>();
+        )
+K_EXPORT_PLUGIN(KcmTwitterFactory("kcmTwitter"))
+
+
+KcmTwitter::KcmTwitter(QWidget *parent, const QVariantList &)
+  : KCModule(KcmTwitterFactory::componentData(), parent)
+{
+  setButtons(Help);
+  setAboutData(new KAboutData("kcmtwitter", 0, ki18n("Twitter Setup"), "0.1",
+                               ki18n("Configure your system for use with Twitter"),
+                               KAboutData::License_GPL, ki18n("Copyright 2010, Sebastian Kügler"),
+                               KLocalizedString(), "sebas@kde.org"));
+
+  setQuickHelp( i18n("<h1>Twitter Setup</h1>"
+              "This module sets up Twitter on your machine, as a desktop widget (the microblog widget) and full app (choqok).<br />"
+              ));
+
+  lbl = new QLabel(this);
+  lbl->setText("Twitter config widget loaded. Yay.");
+
+  QVBoxLayout *top = new QVBoxLayout(this);
+  top->setMargin(0);
+  top->setSpacing(KDialog::spacingHint());
+  top->addWidget(lbl);
+}
+
+//---------------------------------------------------------------------------------------------
+
+void KcmTwitter::load()
+{
+  //lbl->init();
+}
+
+//---------------------------------------------------------------------------------------------
+
+void KcmTwitter::defaults()
+{
+  //lbl->resetCalibration();
+
+  emit changed(true);
+}
+
+//---------------------------------------------------------------------------------------------
+
+#include "kcm.moc"
---
+++ webwelcome/services/twitter/kcm.h
@@ -0,0 +1,44 @@
+/*
+    Copyright 2010 by Sebastian Kügler <sebas@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KCM_H
+#define KCM_H
+
+#include <KCModule>
+#include <QLabel>
+
+#include "ui_config.h"
+
+class KcmTwitter: public KCModule
+{
+  Q_OBJECT
+
+  public:
+    explicit KcmTwitter(QWidget *parent = 0, const QVariantList &list = QVariantList());
+
+    virtual void load();
+    virtual void defaults();
+
+  private:
+    QLabel *lbl;
+    Ui::TWitterConfig m_twitterConfig;
+
+};
+
+#endif
