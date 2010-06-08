ba5a7622b201c7f673f7b6f66ce9465508bbe1d8
Sebastian Kuegler <sebas@miro.vizZzion.net>
2010-06-08 17:14:07 +0200

    simple script to load into Plasma

---
+++ webwelcome/services/twitter/plasma-add-twitter.js
@@ -0,0 +1,19 @@
+var acts = activityIds;
+print(acts);
+for (i = 0; i < acts.length; ++i) {
+  print(acts[i]);
+  var activity = activityById(acts[i])
+  print(activity.name);
+  if (activity.name == "rightDesktop") {
+    print("We have a desktop!");
+    var twitter = activity.addWidget("twitter");
+    twitter.writeConfig("username", "$USER");
+    twitter.writeConfig("password", "$PASS");
+    twitter.showConfigurationInterface();
+    
+    sleep(4000);
+    twitter.remove();
+  }
+}
+
+
