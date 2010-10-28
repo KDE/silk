4b3e43881a5e81ca53d2282e10d5e0c84130a59c
yoms <hormiere.guillaume@gmail.com>
2010-10-29 01:10:27 +0200

    add fonction connect test to test_usergroups classes

--- mediawiki/test/test_usergroups.cpp
+++ mediawiki/test/test_usergroups.cpp
@@ -7,6 +7,8 @@
 #include "mediawiki/usergroups.h"
 #include "mediawiki/test/fake_server/server.h"
 
+Q_DECLARE_METATYPE(QList<UserGroups::Result>)
+
 class test_usergroups : public QObject
 {
     Q_OBJECT
@@ -19,16 +21,19 @@ private slots:
 
     void UserGroupsConnect()
     {
-    //     QString senario("<?xml version=\"1.0\" encoding=\"utf-8\"?><api><login result=\"NeedToken\" token=\"b5780b6e2f27e20b450921d9461010b4\" sessionid=\"17ab96bd8ffbe8ca58a78657a918558e\" /> </api>");
-    //     m_server->addScenario(senario);
-    //     m_server->startAndWait();
-    //     this->m_login = new Login(*m_mediaWiki, "alexTest", "test");
-    //     QSignalSpy spy(m_login, SIGNAL(finishedLogin(bool)));
-    //     QEventLoop loop;
-    //     connect(m_login, SIGNAL(finishedLogin(bool)),&loop,SLOT(quit()));
-    //     loop.exec();
-    //     QCOMPARE(spy.count(), 1);
-    //     QVERIFY(spy.takeFirst().at(0).toBool() == true);
+         qRegisterMetaType< QList<UserGroups::Result> >("QList<UserGroups::Result>" );
+         QString senario("<?xml version=\"1.0\" encoding=\"utf-8\"?><api><query><usergroups><group name=\"*\"><rights><permission>createaccount</permission><permission>read</permission><permission>edit</permission><permission>createtalk</permission><permission>writeapi</permission><permission>centralauth-merge</permission><permission>abusefilter-view</permission><permission>abusefilter-log</permission><permission>abusefilter-log-detail</permission></rights></group><group name=\"user\"><rights><permission>reupload-own</permission><permission>move-rootuserpages</permission><permission>read</permission><permission>edit</permission><permission>createpage</permission><permission>createtalk</permission><permission>writeapi</permission><permission>minoredit</permission><permission>purge</permission><permission>sendemail</permission></rights></group><group name=\"autoconfirmed\"><rights><permission>patrol</permission><permission>reupload</permission><permission>upload</permission><permission>move</permission><permission>collectionsaveasuserpage</permission><permission>collectionsaveascommunitypage</permission><permission>autoconfirmed</permission><permission>movestable</permission><permission>autoreview</permission><permission>skipcaptcha</permission></rights></group><group name=\"bot\"><rights><permission>noratelimit</permission><permission>bot</permission><permission>autoconfirmed</permission><permission>nominornewtalk</permission><permission>autopatrol</permission><permission>suppressredirect</permission><permission>apihighlimits</permission><permission>writeapi</permission><permission>autoreview</permission><permission>skipcaptcha</permission></rights></group><group name=\"sysop\"><rights><permission>suppressredirect</permission><permission>noratelimit</permission><permission>deleterevision</permission><permission>block</permission><permission>createaccount</permission><permission>delete</permission><permission>deletedhistory</permission><permission>deletedtext</permission><permission>undelete</permission><permission>editinterface</permission><permission>editusercss</permission><permission>edituserjs</permission><permission>import</permission><permission>move</permission><permission>move-subpages</permission><permission>move-rootuserpages</permission><permission>patrol</permission><permission>autopatrol</permission><permission>protect</permission><permission>proxyunbannable</permission><permission>rollback</permission><permission>trackback</permission><permission>upload</permission><permission>reupload</permission><permission>reupload-shared</permission><permission>unwatchedpages</permission><permission>autoconfirmed</permission><permission>upload_by_url</permission><permission>ipblock-exempt</permission><permission>blockemail</permission><permission>markbotedits</permission><permission>apihighlimits</permission><permission>browsearchive</permission><permission>versiondetail</permission><permission>movefile</permission><permission>autoreview</permission><permission>stablesettings</permission><permission>movestable</permission><permission>review</permission><permission>tboverride</permission><permission>globalblock-whitelist</permission><permission>nuke</permission><permission>skipcaptcha</permission><permission>override-antispoof</permission><permission>centralnotice-admin</permission><permission>centralnotice-translate</permission><permission>abusefilter-log-detail</permission><permission>abusefilter-revert</permission><permission>abusefilter-view-private</permission></rights></group><group name=\"bureaucrat\"><rights><permission>noratelimit</permission><permission>renameuser</permission><permission>override-antispoof</permission></rights></group><group name=\"checkuser\"><rights><permission>checkuser</permission><permission>checkuser-log</permission></rights></group><group name=\"reviewer\"><rights><permission>patrol</permission><permission>validate</permission><permission>review</permission><permission>autoreview</permission><permission>autoconfirmed</permission><permission>patrolmarks</permission></rights></group><group name=\"steward\"><rights><permission>noratelimit</permission><permission>globalblock</permission><permission>globalunblock</permission><permission>userrights</permission><permission>centralauth-unmerge</permission><permission>centralauth-lock</permission><permission>centralauth-oversight</permission></rights></group><group name=\"accountcreator\"><rights><permission>override-antispoof</permission><permission>tboverride</permission><permission>noratelimit</permission></rights></group><group name=\"import\"><rights><permission>importupload</permission><permission>import</permission></rights></group><group name=\"transwiki\"><rights><permission>import</permission></rights></group><group name=\"ipblock-exempt\"><rights><permission>ipblock-exempt</permission><permission>torunblocked</permission></rights></group><group name=\"oversight\"><rights><permission>deleterevision</permission><permission>hideuser</permission><permission>suppressrevision</permission><permission>suppressionlog</permission><permission>abusefilter-hide-log</permission><permission>abusefilter-hidden-log</permission><permission>hiderevision</permission><permission>oversight</permission></rights></group><group name=\"founder\"><rights><permission>userrights</permission></rights></group><group name=\"rollbacker\"><rights><permission>rollback</permission></rights></group><group name=\"confirmed\"><rights><permission>patrol</permission><permission>reupload</permission><permission>upload</permission><permission>move</permission><permission>collectionsaveasuserpage</permission><permission>collectionsaveascommunitypage</permission><permission>autoconfirmed</permission><permission>movestable</permission><permission>autoreview</permission><permission>skipcaptcha</permission></rights></group><group name=\"autoreviewer\"><rights><permission>autopatrol</permission></rights></group><group name=\"researcher\"><rights><permission>browsearchive</permission><permission>deletedhistory</permission><permission>apihighlimits</permission></rights></group><group name=\"abusefilter\"><rights><permission>abusefilter-modify</permission></rights></group></usergroups></query></api>");
+         m_server->addScenario(senario);
+         m_server->startAndWait();
+         this->m_usergroups = new UserGroups(*m_mediaWiki);
+         QSignalSpy spy(m_usergroups, SIGNAL(finished(QList<UserGroups::Result>)));
+         QEventLoop loop;
+         connect(m_usergroups, SIGNAL(finished(QList<UserGroups::Result> const &)),&loop,SLOT(quit()));
+         loop.exec();
+         QCOMPARE(spy.count(), 1);
+         QVariantList arguments = spy.takeFirst();
+         QList<UserGroups::Result> results = arguments.at(0).value< QList<UserGroups::Result> >();
+         QCOMPARE( results.size(), 20);
     }
 
 
@@ -46,8 +51,7 @@ private:
 
 #endif // TEST_USERGROUPS_H
 
-#include "test_usergroups.moc"
-
 QTEST_MAIN(test_usergroups)
+#include "test_usergroups.moc"
 
 
