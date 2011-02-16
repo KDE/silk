a5d1f4ddc5e2b6e3096c45fbfa58333a593e2925
skaboy <bussenot.robin@gmail.com>
2011-02-16 11:44:31 +0100

    Add comments on revision.h

--- mediawiki/libmediawiki/revision.h
+++ mediawiki/libmediawiki/revision.h
@@ -31,117 +31,117 @@ public:
      * @param other an other revision
      */
     Revision & operator=(Revision other);
+
     /**
-     * @param Revision Id
-     */
+    * @brief Set the Revision ID
+    * @param Id Revision ID
+    **/
     void setRevId(int id);
 
     /**
-     * @brief
+     * @brief get the Revision ID
      */
     int revId() const;
 
     /**
-     * @brief
-     * @param
+     * @briefSet the parent ID
+     * @param ID ID of parent
      */
     void setParentId(int id);
 
     /**
-     * @brief
+     * @brief get parent ID
      */
     int parentId() const;
 
     /**
-     * @brief
-     * @param
+     * @brief Set the size of the revision text in bytes
+     * @param size size of revision text
      */
     void setSize(int size);
 
     /**
-     * @brief
+     * @brief get the size of the revision text
      */
     int size() const;
 
     /**
-     * @brief
-     * @param
+     * @brief Whether the revision was a minor edit
+     * @param minor
      */
     void setMinor(const QString &  minor);
 
     /**
-     * @brief
-     * @param
+     * @brief Get Minor  // Missing doc
      */
     QString minor() const;
 
 
     /**
-     * @brief
+     * @brief  Get the date and time the revision was made
      */
     QDateTime timestamp() const;
 
     /**
-     * @brief
-     * @param
+     * @brief Set the date and time the revision was made
+     * @param timestamp
      */
     void setTimestamp(const QDateTime & timestamp);
 
     /**
-     * @brief
-     * @param
+     * @brief Get the user who made the revision
      */
     QString user() const;
 
     /**
-     * @brief
-     * @param
+     * @brief Set the user who made the revision
+     * @param user revision owner
      */
     void setUser(const QString & user);
 
     /**
-     * @brief
+     * @brief The revision content. If set, the maximum limit will be 10 times as low
      */
     QString content() const;
 
     /**
-     * @brief
-     * @param
+     * @brief Set the revision content
+     * @param content
      */
     void setContent(const QString & content);
 
 
     /**
-     * @brief
+     * @brief Get the edit comment
      */
     QString comment() const;
 
     /**
-     * @brief
-     * @param
+     * @brief Set the edit comment
+     * @param Comment the edit comment
      */
     void setComment(const QString &  com);
 
     /**
-     * @brief
-     * @param
+     *  @brief Set parse tree for revision content
+     * @param parseTree
      */
     void setParseTree(const QString &  parseTree);
 
 
     /**
-     * @brief
+     * @brief Get parseTree
      */
     QString parseTree() const;
 
     /**
-     * @brief
-     * @param
+     * @brief Set rollback token
+     * @param rollback token needed to rollback edits from a page
      */
     void setRollback(const QString &  rollback);
 
     /**
-     * @brief
+     * @brief get rollback token
      */
     QString rollback() const;
 
