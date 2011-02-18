df372f4ffc0de9f9455e69c903c766b02afd3ae2
alexandre <alex.mendes1988@gmail.com>
2011-02-18 14:15:30 +0100

    Add gui and update wmwindow.

--- mediawiki/kipi-plugin/wmwindow.cpp
+++ mediawiki/kipi-plugin/wmwindow.cpp
@@ -1,12 +1,38 @@
+#include <KDialog>
+#include "ui_wmwindow.h"
+#include <libkipi/interface.h>
 #include "wmwindow.h"
 
 using namespace KIPIWikiMediaPlugin;
+
 WMWindow::WMWindow(KIPI::Interface* interface, const QString &tmpFolder,
-                   bool import, QWidget* /*parent*/)
-        : KDialog(0)
+                  bool import, QWidget* /*parent*/)
+       : KDialog(0), ui(new Ui::WMWindow)
 {
+   ui->setupUi(this);
+   m_tmpDir      = tmpFolder;
+   m_interface   = interface;
+   m_import      = import;
+   this->setButtonText(KDialog::Ok, i18n("Upload"));
 }
 void WMWindow::reactivate()
 {
-    show();
+   show();
+}
+
+WMWindow::~WMWindow()
+{
+    delete ui;
+}
+
+void WMWindow::changeEvent(QEvent *e)
+{
+    KDialog::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        ui->retranslateUi(this);
+        break;
+    default:
+        break;
+    }
 }
--- mediawiki/kipi-plugin/wmwindow.h
+++ mediawiki/kipi-plugin/wmwindow.h
@@ -2,15 +2,27 @@
 #define WMWINDOW_H
 #include <KDialog>
 #include <libkipi/interface.h>
+
+namespace Ui {
+    class WMWindow;
+}
 namespace KIPIWikiMediaPlugin
 {
-class WMWindow : public KDialog
-{
+class WMWindow : public KDialog {
+    Q_OBJECT
 public:
     WMWindow(KIPI::Interface *interface, const QString& tmpFolder,
-             bool import, QWidget *parent);
+                     bool import, QWidget *parent);
+    ~WMWindow();
     void reactivate();
-};
+protected:
+    void changeEvent(QEvent *e);
 
+private:
+    Ui::WMWindow *ui;
+    bool                      m_import;
+    QString                   m_tmpDir;
+    KIPI::Interface          *m_interface;
+};
 }
 #endif // WMWINDOW_H
