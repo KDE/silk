d7c969a19c44379ce07a8a6008a125d65c4e019f
Gilles Caulier <caulier.gilles@gmail.com>
2011-03-24 07:59:24 +0100

    prepare libmediawwiki for production

---
+++ mediawiki/libmediawiki/libmediawiki/version.h.cmake
@@ -0,0 +1,33 @@
+/** ===========================================================
+ *
+ * This file is a part of digiKam project
+ * <a href="http://www.digikam.org">http://www.digikam.org</a>
+ *
+ * @date   2004-09-09
+ * @brief  KIPI release ID header
+ *
+ * @author Copyright (C) 2004-2010 by Gilles Caulier
+ *         <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef KIPI_VERSION_H
+#define KIPI_VERSION_H
+
+static const char kipi_version[]     = "${KIPI_LIB_VERSION_STRING}";
+static const int kipi_binary_version =  ${KIPI_LIB_SO_CUR_VERSION};
+#define KIPI_VERSION ${KIPI_LIB_VERSION_ID}
+
+#endif // KIPI_VERSION_H
+
